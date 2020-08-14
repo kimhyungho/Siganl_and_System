635195e8ed06f168671ee9e687f1ca98939553cd
kimhyungho <68440536+kimhyungho@users.noreply.github.com>
2020-08-14 23:24:15 +0900

    Add files via upload

---
+++ 07.sounds_filtering/sound_lowpass_filtering.m
@@ -0,0 +1,21 @@
+[x,fs]=audioread('guitar.wav');
+[N,a]=size(x);
+t=0 : 1/fs : N/fs-1/fs ;
+plot(t, x)
+X=fftshift(fft(x));
+f=-N/2:N/2-1;
+figure
+plot(f, abs(X))
+axis([-20000 20000 0 600])
+R=12000;
+H=zeros(1, N);
+H(N/2-R:N/2+R)=1;
+figure
+plot(H)
+axis([0 250000 -0.2 1.2])
+Y = X .* H ';
+figure
+plot(t, Y)
+x2=ifft(fftshift(Y));
+figure
+plot(t, x2)
